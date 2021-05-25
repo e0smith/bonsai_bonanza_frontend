363ae943ecf9da7adc49b6f987c19cb58651ea13
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-24 22:24:20 -0400

    tree names listed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function fetchTrees() {
+    const treesContainer = document.getElementById("trees-container")
     fetch("http://localhost:3000/trees")
     .then(r => r.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.forEach(function(tree){
+            treesContainer.innerHTML += `<li>${tree.name}</li>`
+        })
+    })
     .catch(err => console.warn(err))
 }
