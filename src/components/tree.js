c848afe47a4a0f18bab60bc584c6f3aae825185f
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-25 18:01:56 -0400

    tree constructor

---
+++ src/components/tree.js
@@ -0,0 +1,15 @@
+class Tree {
+
+    constructor({id, name, pruning, wiring, watering, repotting, propagation, disease, placement, fertilizing}) {
+        this.id = id
+        this.name = name
+        this.pruning = pruning
+        this.wiring = wiring
+        this.watering = watering
+        this.repotting = repotting
+        this.propagation = propagation
+        this.disease = disease
+        this.placement = placement
+        this.fertilizing = fertilizing
+    }
+}
