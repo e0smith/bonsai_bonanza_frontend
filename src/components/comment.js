22c531be22deabbcea09cb9c4410e4573d7cc1d0
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-29 17:36:30 -0400

    comment render

--- src/components/comment.js
+++ src/components/comment.js
@@ -13,21 +13,22 @@ class Comment {
     //     fetch(`${treeURL}/${id}/comments`)
     //     .then(r => r.json())
     //     .then(comments => {
-    //         comments.forEach(comment => new Comment(comment))
-    //             // Comment.render()
+    //         comments.forEach(comment => new Comment(comment.id, comment.comment, comment.tree_id))
     //     })
     //     .catch(error => console.error(error))
     // }
 
-    // static render() {
-    //     let treeId = id
-    //     let x = Comment.all.forEach(comment => comment.id.tree_id == treeId)
-    //     return(`<li id="comment-${this.id}" data-id=${this.id}>
-    //     <span>${this.comment}</span>
-    //     <button data-action='delete'>X</button>
-    //     </li>`
-    //     )
-    // }
+    static render(id) {
+        let a = document.createElement('li')
+        let treeId = id
+        let x = Comment.all.filter(comment => comment.id.tree_id == treeId)
+        let y = x.map(c => c.id.comment)
+        let z = y.forEach(c => a.innerHTML += `<li data-id=${treeId}>
+        <span>${c}</span>
+        <button data-action='delete'>X</button>
+        </li>`)
+        showDiv.append(a)
+    }
 
     static createComment(event, commentInput) {
         let id = event.target.dataset.id
